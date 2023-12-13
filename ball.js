7cc2783bf02ab9a3348848e5b0793f72aeea11b9
U2girl <elycias@yahoo.com>
2023-12-12 19:27:17 -0500

    magicball8

--- ball.js
+++ ball.js
@@ -17,6 +17,25 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
       return;
     }
   
+
+    app.get('/magic/:question', (req, res) => {
+        const question = req.params.question.replace(/%20/g, ' ');
+        const responses = [
+          "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+          "You may rely on it", "As I see it yes", "Most likely", "Outlook good",
+          "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+          "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+          "Don't count on it", "My reply is no", "My sources say no",
+          "Outlook not so good", "Very doubtful"
+        ];
+        const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+
+  res.send(`<h1>${question}? ${randomResponse}</h1>`);
+});
+
+
+
+
     const tipAmount = (total * tipPercentage) / 100;
     res.send(`Your tip will be: $${tipAmount.toFixed(2)}`);
   });
