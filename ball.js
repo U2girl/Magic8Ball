93f730ea852cd7c7b4b1b713796a11d2a583a4d2
U2girl <elycias@yahoo.com>
2023-12-12 19:08:48 -0500

    tipcalc

--- ball.js
+++ ball.js
@@ -7,6 +7,21 @@ const port = 3000;
 app.get('/greeting', (req, res) => {
   res.send('Hello Stranger!');
 });
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = parseFloat(req.params.total);
+    const tipPercentage = parseFloat(req.params.tipPercentage);
+    
+    if (isNaN(total) || isNaN(tipPercentage)) {
+      res.send('Invalid input. Please provide valid numbers.');
+      return;
+    }
+  
+    const tipAmount = (total * tipPercentage) / 100;
+    res.send(`Your tip will be: $${tipAmount.toFixed(2)}`);
+  });
+
+
 app.listen(port, () => {
   console.log(`Example server is running at http://localhost:${port}`);
 });
